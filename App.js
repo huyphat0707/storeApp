ffd810baddd0d2024441f91e766e7c769cbf4d58
phat <haphat0707@gmail.com>
2020-09-02 13:47:01 +0700

    animation

--- App.js
+++ App.js
@@ -1,21 +1,25 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+import CategoryListItem from "./components/CategoryListItem";
 
-export default function App() {
-  return (
+export default class App extends React.Component {
+  render() {
+    return(
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      {/* <Text>Open up App.js to start working on your app!</Text> */}
+      <CategoryListItem />
       <StatusBar style="auto" />
     </View>
-  );
+    );
+  }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
---
+++ components/CategoryListItem.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { View, Text, Image } from 'react-native';
+import SkillImage from '../assets/image/sport.png'
+
+export default function CategoryListItem(props){
+  return
+  <View>
+    <Text>
+      CategoryListItem
+    </Text>
+  </View>
+
+}
